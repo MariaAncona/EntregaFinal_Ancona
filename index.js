89e58ed80bc47fa744f74bbec29d8314afc6d481
MariaAncona <maria.ancona18@gmail.com>
2024-09-08 16:46:30 -0600

    imagenes

---
+++ index.js
@@ -0,0 +1,64 @@
+const categorias =[
+    {
+    id: 1,
+    nombre: "Bromelias",
+    imagen: "https://drive.google.com/file/d/1VU1_qNGmgULcCv9TWLuhto1_0dgvUjfp/view?usp=drive_link"
+    },
+
+    {
+    id: 2,
+    nombre: "Bonsai",
+    imagen: "https://drive.google.com/file/d/1PJhjD1Lbydtg8HYlOwDKRmg-9lK4xNGS/view?usp=drive_link"
+    },
+
+    {
+    id: 3,
+    nombre: "Suculentas",
+    imagen: "https://drive.google.com/file/d/1lv4PlSb0F0YFECaoHVj1VKLK5LH-TYrj/view?usp=drive_link"
+    },
+
+    {
+    id: 4,
+    nombre: "Carnivoras",
+    imagen: "https://drive.google.com/file/d/1KDhet0FJHDSmBsJTtI_hwk7NHkzd_ftY/view?usp=drive_link"
+    },
+
+    {
+    id: 5,
+    nombre: "Orquídeas",
+    imagen: "https://drive.google.com/file/d/132igpWgY3SmBQSultl1U3XilJTLHsrsh/view?usp=drive_link"
+    },
+
+
+    {
+    id: 6,
+    nombre: "Hojas",
+    imagen: "https://drive.google.com/file/d/180Kw7i63aPJPD_JYYGtyY0GazwiJ74hI/view?usp=drive_link"
+    },
+
+
+    {
+    id: 7,
+    nombre: "Huerta",
+    imagen: "https://drive.google.com/file/d/1Z64dXg9J86ZIub-73rBnyPAe_VorbGXm/view?usp=drive_link"
+    },
+
+
+    {
+    id: 8,
+    nombre: "Flores",
+    imagen: "https://drive.google.com/file/d/1SSybiBFcOkGGQ_kUOORpsc9JrGsWUcOK/view?usp=drive_link"
+    },
+
+    {
+    id: 9,
+    nombre: "Enredadera",
+    imagen: "https://drive.google.com/file/d/1sMkrDo9_3oF7yLMrdTTAzg_I3cS2cSK9/view?usp=drive_link"
+    },
+
+    {
+    id: 10,
+    nombre: "Buena Suerte",
+    imagen: "https://drive.google.com/file/d/1Hm9PWAVg2L9JFlV_5kvpzHd-esYaLQTm/view?usp=drive_link"
+    }
+]
