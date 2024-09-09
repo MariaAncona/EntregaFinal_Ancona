0510057202a0d205a3527014204b627750ae8c2c
MariaAncona <maria.ancona18@gmail.com>
2024-09-08 21:00:23 -0600

    tienda

---
+++ tienda.js
@@ -0,0 +1,25 @@
+const seccionTienda = document.getElementById ('seccionTienda');
+
+fetch("/productos.json")
+    .then((response) => response.json())
+    .then((data) => {
+
+        for (const producto of data){
+            const planta = document.createElement('div');
+            planta.innerHTML = `
+            <div class = "plantas">
+                <img class = "img-planta" src= ${producto.Imagen} alt=${producto.Nombre}/>
+                <div class ="txt-planta">
+                    <h5 class = "nombre">${producto.Nombre} </h5>
+                    <p class = "precio"> $${producto.Precio}.- </p>
+                    <a href="#" class= "btn-compra"> Agregar </a>
+                </div>
+            </div>
+                `
+
+                seccionTienda.appendChild(planta);
+
+        };
+    })
+    .catch(console.error());
+
