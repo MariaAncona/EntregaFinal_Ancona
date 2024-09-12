070e0ff60d684d0731cc0015b83d782ad94641e5
MariaAncona <maria.ancona18@gmail.com>
2024-09-11 21:45:10 -0600

    tienda

--- tienda.js
+++ tienda.js
@@ -1,25 +1,55 @@
-const seccionTienda = document.getElementById ('seccionTienda');
+
+const seccionTienda = document.getElementById('seccionTienda');
+const selectCategoria = document.getElementById('selectCategoria');
+let productos = [];
+
 
 fetch("/productos.json")
-    .then((response) => response.json())
-    .then((data) => {
-
-        for (const producto of data){
-            const planta = document.createElement('div');
-            planta.innerHTML = `
-            <div class = "plantas">
-                <img class = "img-planta" src= ${producto.Imagen} alt=${producto.Nombre}/>
-                <div class ="txt-planta">
-                    <h5 class = "nombre">${producto.Nombre} </h5>
-                    <p class = "precio"> $${producto.Precio}.- </p>
-                    <button class= "btn-compra"> Agregar </button>
-                </div>
-            </div>
-                `
-
-                seccionTienda.appendChild(planta);
-
-        };
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        mostrarProductos(productos); 
     })
-    .catch(console.error());
+    .catch(console.error);
+
+function mostrarProductos(productosFiltrados) {
+    seccionTienda.innerHTML = ''; 
+
+    if (productosFiltrados.length === 0) {
+        seccionTienda.innerHTML = '<p>No se encontraron productos</p>';
+        return;
+    }
+
+    productosFiltrados.forEach(producto => {
+        const planta = document.createElement('div');
+        planta.innerHTML = `
+        <div class="plantas">
+            <img class="img-planta" src="${producto.Imagen}" alt="${producto.Nombre}" />
+            <div class="txt-planta">
+                <h5 class="nombre">${producto.Nombre}</h5>
+                <p class="precio">$${producto.Precio}.-</p>
+                <button class= "btn-compra"> Agregar </button>
+            </div>
+        </div>
+        `;
+        seccionTienda.appendChild(planta);
+    });
+}
+
+
+function filtrarPorCategoria() {
+    const categoriaSeleccionada = selectCategoria.value.toLowerCase();
+
+   
+    if (categoriaSeleccionada === 'non') {
+        mostrarProductos(productos);
+    } else {
+        const productosFiltrados = productos.filter(producto =>
+            producto.categoria.nombre.toLowerCase() === categoriaSeleccionada
+        );
+        mostrarProductos(productosFiltrados); 
+    }
+}
+
+selectCategoria.addEventListener('change', filtrarPorCategoria);
 
